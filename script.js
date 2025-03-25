a854af2693fdccbe8fefa1c8a3f19b25ec66b97f
Jpfpp12 <118419073+Jpfpp12@users.noreply.github.com>
2025-03-25 20:13:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,36 +1,36 @@
 //complete this code
 class Person {
-	  constructor(name, age) {
-    this._name = name;
-	this._age = age;   
+	 constructor(name, age) {
+    this._name = name; 
+    this._age = age;   
   }
+
+  // Getter for name
   get name() {
     return this._name;
   }
   set age(age) {
-    this._age = age; 
+    this._age = age;
   }
-
 }
 
 class Student extends Person {
-	  constructor(name, age) {
-    super(name, age);
+  constructor(name, age) {
+    super(name, age); 
   }
   study() {
     console.log(`${this.name} is studying`);
   }
-
 }
 
 class Teacher extends Person {
-	  constructor(name, age) {
+ constructor(name, age) {
+    super(name, age);
   }
   teach() {
     console.log(`${this.name} is teaching`);
   }
 }
-
 // Do not change the code below this line
 window.Person = Person;
 window.Student = Student;
